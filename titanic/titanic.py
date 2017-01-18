84ed35a849552bc32916adeed008841ef9582ac4
Kai <kai.hirsinger@gmail.com>
2017-01-19 09:29:25 +1100

    added exploration notebook, predictions, requirements file and latest prediction script changes

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -1,14 +1,25 @@
+import numpy as np
 import pandas as pd
 
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
 
 
 TRAIN = 'train.csv'
 TEST  = 'test.csv'
 
-X = ['Sex', 'Pclass', 'Embarked', 'Fare']
+X = [
+    'Sex',
+    'Pclass',
+    'Fare',
+    'Age',
+    'SibSp',
+    'Parch',
+    'FamilySize'
+]
 Y = 'Survived'
 ID = 'PassengerId'
 
@@ -35,15 +46,28 @@ def embarked_to_integer(embarked):
         return 0
 
 
-def clean_data(data):
+def clean_data(data, mode='train'):
     data['Sex'] = data['Sex'].apply(gender_to_integer)
     data['Embarked'] = data['Embarked'].apply(embarked_to_integer)
+
+    data['FamilySize'] = data['SibSp'] + data['Parch']
 
     return data.fillna(0)
 
 
+def equalize_target_class(data, target_class):
+    return
+
+
 def fit_model(x, y):
-    model = LinearSVC()
+    model = GridSearchCV(
+        estimator  = LinearSVC(),
+        n_jobs = -1,
+        param_grid = dict(
+            C = np.logspace(-6, -1, 10)
+        ),
+    )
+    #model = RandomForestClassifier(n_estimators = 100)
     model.fit(x, y)
     return model
 
@@ -60,12 +84,14 @@ def evaluate_model(model, x, true_y):
 def main():
     #Read training data
     train = clean_data(
-        pd.read_csv('train.csv')
+        pd.read_csv('train.csv'),
+        mode = 'train'
     )
 
     #Read test data
     test = clean_data(
-        pd.read_csv('test.csv')
+        pd.read_csv('test.csv'),
+        mode = 'test'
     )
 
     if MODE == 'DEVELOP':
@@ -79,7 +105,7 @@ def main():
 
         model = fit_model(x_train, y_train)
 
-        print evaluate_model(model, x_test, y_test)
+        print(evaluate_model(model, x_test, y_test))
 
     elif MODE == 'FINAL':
         model = fit_model(train[X], train[Y])
@@ -87,7 +113,7 @@ def main():
         test[OUTPUT].to_csv('predictions.csv', index=False)
 
     else:
-        print "ERROR: Bad MODE specified; choose one of DEVELOP or FINAL"
+        print("ERROR: Bad MODE specified; choose one of DEVELOP or FINAL")
 
 if __name__ == '__main__':
     main()
